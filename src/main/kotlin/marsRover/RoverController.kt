ca93ac775b90d681334b769c85c82412a8d4c60f
Lester Han <lehan@thoughtworks.com>
2019-03-26 13:37:16 -0400

    Add command processing

--- src/main/kotlin/marsRover/RoverController.kt
+++ src/main/kotlin/marsRover/RoverController.kt
@@ -1,6 +1,16 @@
 package marsRover
 
-class RoverController(val plateauLength: Int, val plateauWidth: Int) {
-
-
+class RoverController(
+    val plateauDimention: Vector,
+    val rover: Rover
+) {
+    fun commandRover(commandString: String) {
+        for(command in commandString){
+            when (command) {
+                'M' -> rover.move()
+                'L' -> rover.turnLeft()
+                'R' -> rover.turnRight()
+            }
+        }
+    }
 }
--- src/test/kotlin/marsRover/RoverControllerTest.kt
+++ src/test/kotlin/marsRover/RoverControllerTest.kt
@@ -2,8 +2,52 @@ package marsRover
 
 import io.kotlintest.should
 import io.kotlintest.specs.StringSpec
+import io.mockk.mockk
+import io.mockk.verify
 import marsRover.Direction.*
 
-class RoverControllerTest: StringSpec ({
+class RoverControllerTest : StringSpec({
+
+    "commandRover" should {
+        "move rover when given M command" {
+            val rover: Rover = mockk<Rover>(relaxed = true)
+            val rc = RoverController(Vector(5, 5), rover)
+
+            rc.commandRover("M")
+
+            verify (exactly = 1) { rover.move() }
+        }
+        "turn left when given L command" {
+            val rover: Rover = mockk<Rover>(relaxed = true)
+            val rc = RoverController(Vector(5, 5), rover)
+
+            rc.commandRover("L")
+
+            verify (exactly = 1) { rover.turnLeft() }
+        }
+        "turn right when given R command" {
+            val rover: Rover = mockk<Rover>(relaxed = true)
+            val rc = RoverController(Vector(5, 5), rover)
+
+            rc.commandRover("R")
+
+            verify (exactly = 1) { rover.turnRight() }
+        }
+        "respond to multiple commands" {
+            val rover: Rover = mockk<Rover>(relaxed = true)
+            val rc = RoverController(Vector(5, 5), rover)
+
+            rc.commandRover("MLRMMR")
+
+            verify {
+                rover.move()
+                rover.turnLeft()
+                rover.turnRight()
+                rover.move()
+                rover.move()
+                rover.turnRight()
+            }
+        }
+    }
 
 })
